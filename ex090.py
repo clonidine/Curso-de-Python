f1012b792988ba577f4b5648b34d636c313f030c
kysmi <86500701+kysmi@users.noreply.github.com>
2021-06-26 18:51:23 -0300

    Delete ex090.py

--- ex090.py
+++
@@ -1,4 +0,0 @@
-aluno = {"nome": str(input('Nome: '))}
-aluno['media'] = float(input(f'Média de {aluno["nome"]}: '))
-aluno['situacao'] = 'aprovado' if aluno['media'] >= 7.0 else 'reprovado' if aluno['media'] < 6.0 else 'recuperação'
-print(f'- nome é igual a {aluno["nome"]}\n- média é igual a {aluno["media"]}\n- situação é igual a {aluno["situacao"]}')
