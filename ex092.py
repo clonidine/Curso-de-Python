4d2a4f8efa4d3d9f5b04cd84f697fb186573518e
kysmi <86500701+kysmi@users.noreply.github.com>
2021-06-29 17:31:22 -0300

    Update ex092.py

--- ex092.py
+++ ex092.py
@@ -8,11 +8,11 @@ infos = dict()
 infos['nome'] = str(input('Nome: '))
 infos['nascimento'] = int(input('Ano de Nascimento: '))
 infos['carteira'] = int(input('Carteira de trabalho ( 0 não tem ): '))
-infos['idade'] = datetime.now().year - infos['nascimento']
+infos['idade'] = ano - infos['nascimento']
 if infos['carteira'] != 0:
     infos['contratação'] = int(input('Ano de Contratação: '))
     infos['salário'] = float(input('Salário: R$'))
-    infos['aposentadoria'] = infos['idade'] + ((infos['carteira'] + 35) - datetime.now().year)
+    infos['aposentadoria'] = infos['idade'] + ((infos['contratação'] + 35) - ano)
 
 for k, v in infos.items():
-    print(f'{k} tem o valor {v}')
+    print(f'  - {k} tem o valor {v}')
