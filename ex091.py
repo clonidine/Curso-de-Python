8028c34ae9fda6f8a3ed07f732098d10e45ff519
kysmi <86500701+kysmi@users.noreply.github.com>
2021-06-29 16:06:47 -0300

    Add files via upload

---
+++ ex091.py
@@ -0,0 +1,24 @@
+# Exercício Python #091 - Jogo de Dados em Python
+
+from random import randint
+from time import sleep
+from operator import itemgetter
+
+jogo = {
+    'jogador1': randint(1, 6),
+    'jogador2': randint(1, 6),
+    'jogador3': randint(1, 6),
+    'jogador4': randint(1, 6)
+}
+
+ranking = []
+for k, v in jogo.items():
+    print(f'{k} tirou {v} no dado.')
+    sleep(1)
+print('-=' * 30)
+print('== RANKING DOS JOGADORES ==')
+ranking = sorted(jogo.items(), key=itemgetter(1), reverse=True)
+
+for i, v in enumerate(jogo.items()):
+    print(f'{i+1} lugar: {v[0]} com {v[1]}')
+    sleep(1)
